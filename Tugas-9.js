d63c553399209ff766e692875434668d8d3e0dae
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 09:09:09 +0700

    Tugas_9_JS
    Tugas_9_JS

---
+++ Tugas-9.js
@@ -0,0 +1,11 @@
+function tugas9() {
+    var item = {
+        nama: "Custom Case",
+        harga: 150000,
+        type: "Glossy"
+    }
+    for (var y in item) {
+        console.log(item[y]);
+    }
+}
+tugas9()
